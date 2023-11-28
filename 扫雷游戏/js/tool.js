f7caf622b1f740c91f5ab5fd578ea3e9c5442df1
tomato-w7 <152295877+tomato-w7@users.noreply.github.com>
2023-11-29 03:23:14 +0800

    Add files via upload

---
+++ 扫雷游戏/js/tool.js
@@ -0,0 +1,14 @@
+/**
+ * 工具函数
+ */
+
+
+//获取dom元素
+function $(selector){
+    return document.querySelector(selector);
+}
+
+//获取多个dom元素
+function $$(selector){
+    return document.querySelectorAll(selector);
+}
